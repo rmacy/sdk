ff31a06c1a46bf8f00c353e3787d64661b73c8b0
Ryan Macy <rmacy@bitr0t.local>
2019-09-15 20:53:26 -0400

    add useAuth hook

---
+++ packages/react-auth/src/useAuth.tsx
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+
+import { AuthContext } from './AuthContext';
+
+const useAuth = () => {
+  const ctx = useContext(AuthContext);
+
+  return ctx;
+};
+
+export { useAuth };
